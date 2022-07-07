5664bf0307d7f2f0e200a6a36c09c3f437b79708
Emilis Veliullajev <prefis@pm.me>
2022-07-07 23:09:50 +0300

    Add score limit

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Generate random computer pick
 function computerPlay() {
 	randomNum = Math.random();
 	if (randomNum <= 0.333) {
@@ -29,71 +30,84 @@ function playRound(computer, player) {
 	return result;
 }
 
-function game(rounds) {
+function game() {
 	let playerScore = 0;
 	let computerScore = 0;
 
+    let maxScore = 5; // At what score pick a winner
+
 	document.querySelector(".buttons").addEventListener("click", function (e) {
-		let selection = e.target.getAttribute("id");
-		if (selection == null) {
-			return;
+
+        // Check who won
+		if (playerScore == maxScore && computerScore == maxScore) {
+			console.log("Tie! Nobody won...");
+		} else if (playerScore == maxScore) {
+			console.log("Player won!");
+		} else if (computerScore == maxScore) {
+			console.log("Computer won!");
+		} else { // Continue game if score below "max-score"
+			let selection = e.target.getAttribute("id");
+			if (selection == null) {
+				return;
+			}
+
+			// Getting game result
+			let result = [];
+			if (selection == "rock") {
+				result = playRound(computerPlay(), "rock");
+			} else if (selection == "paper") {
+				result = playRound(computerPlay(), "paper");
+			} else if (selection == "scissors") {
+				result = playRound(computerPlay(), "scissors");
+			}
+
+			let textResult = document.querySelector("#textResult");
+
+			// Count score and show text result
+			if (result[2] == 1) {
+				computerScore++;
+				textResult.textContent = "Computer win!";
+			} else if (result[2] == 2) {
+				playerScore++;
+				textResult.textContent = "Player win!";
+			} else {
+				textResult.textContent = "Tie!";
+			}
+
+			// Show score
+			document.querySelector("#playerScore").innerText = playerScore;
+			document.querySelector("#computerScore").innerText = computerScore;
+
+			// Show picks
+			const rock = "✊";
+			const paper = "✋";
+			const scissors = "✌️";
+
+			let pPick = document.querySelector("#playerPick");
+			let cPick = document.querySelector("#computerPick");
+
+			// Player pick
+			if (selection == "rock") {
+				pPick.innerText = rock;
+			} else if (selection == "paper") {
+				pPick.innerText = paper;
+			} else if (selection == "scissors") {
+				pPick.innerText = scissors;
+			}
+			// Computer pick
+			if (result[0] == "rock") {
+				cPick.innerText = rock;
+			} else if (result[0] == "paper") {
+				cPick.innerText = paper;
+			} else if (result[0] == "scissors") {
+				cPick.innerText = scissors;
+			}
+
+			console.log(typeof playerScore);
 		}
-
-		// Getting game result
-		let result = [];
-		if (selection == "rock") {
-			result = playRound(computerPlay(), "rock");
-		} else if (selection == "paper") {
-			result = playRound(computerPlay(), "paper");
-		} else if (selection == "scissors") {
-			result = playRound(computerPlay(), "scissors");
-		}
-
-        let textResult = document.querySelector("#textResult");
-
-		// Count score and show text result
-		if (result[2] == 1) {
-			computerScore++;
-            textResult.textContent = "Computer win!";
-		} else if (result[2] == 2) {
-			playerScore++;
-            textResult.textContent = "Player win!";
-		} else {
-            textResult.textContent = "Tie!";
-        }
-
-		// Show score
-		document.querySelector("#playerScore").innerText = playerScore;
-		document.querySelector("#computerScore").innerText = computerScore;
-
-		// Show picks
-		const rock = "✊";
-		const paper = "✋";
-		const scissors = "✌️";
-
-		let pPick = document.querySelector("#playerPick");
-		let cPick = document.querySelector("#computerPick");
-
-		// Player pick
-		if (selection == "rock") {
-			pPick.innerText = rock;
-		} else if (selection == "paper") {
-			pPick.innerText = paper;
-		} else if (selection == "scissors") {
-			pPick.innerText = scissors;
-		}
-		// Computer pick
-		if (result[0] == "rock") {
-			cPick.innerText = rock;
-		} else if (result[0] == "paper") {
-			cPick.innerText = paper;
-		} else if (result[0] == "scissors") {
-			cPick.innerText = scissors;
-		}
-
-
-		console.log(result); // debug
 	});
 }
 
-game(); // Start game function
+// Start game function
+
+game();
